6e846d4f76c1617bb034ff2bb60b767c01ca7688
zemlianin <detulie@mail.ru>
2022-12-12 00:47:26 +0300

    add-serialize

--- bb_back/bb_back/core/views/shared/games_list.py
+++ bb_back/bb_back/core/views/shared/games_list.py
@@ -10,10 +10,24 @@ from bb_back.core.utils.view_utils import response
 from bb_back.core.views.utils.base_serializers import BaseResponseSerializer
 
 
+class GameSerializer(serializers.Serializer):
+    Name = serializers.CharField(max_length=30)
+    # Organizater = models.ForeignKey(User, on_delete=models.CASCADE)
+    # created_at = serializers.DateTimeField(default=datetime.now())
+    # players = .ManyToManyField(User)
+    # finish_at = serializers.DateTimeField(default=datetime.now() + timedelta(days=1))
+    is_active = serializers.BooleanField(default=True)
+
+
+class GameResponseSerializer(BaseResponseSerializer):
+    response_data = GameSerializer()
+
+
 class GameView(APIView):
-    #   @swagger_auto_schema(
-    #       request_body=RegistrationRequestSerializer,
-    #       responses={status.HTTP_200_OK: RegistrationResponseSerializer})
+    @swagger_auto_schema(
+        request_body=GameSerializer,
+        responses={status.HTTP_200_OK: GameResponseSerializer},
+    )
     def post(self, request):
         # request_data = RegistrationRequestSerializer(data=request.data)
         return response(
