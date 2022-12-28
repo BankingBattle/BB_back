f09a20c6bfa09a5f190eeee2b0bd520763342fc0
Daniil <pimanov.d@gmail.com>
2022-12-28 09:11:38 +0300

    fixed urls

--- bb_back/bb_back/urls.py
+++ bb_back/bb_back/urls.py
@@ -15,15 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, re_path
-
-from bb_back.settings import API_PREFIX, API_VERSION
-from bb_back.core import views
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 from rest_framework_simplejwt.views import (TokenObtainPairView,
                                             TokenRefreshView, TokenVerifyView)
 
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
+from bb_back.core import views
+from bb_back.settings import API_PREFIX, API_VERSION
 
 schema_view = get_schema_view(
     openapi.Info(
