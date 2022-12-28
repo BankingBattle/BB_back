63580daeeac72256262e0d7cfb3d5b65b9effce9
Daniil <pimanov.d@gmail.com>
2022-12-28 14:42:58 +0300

    allowed host fix

--- bb_back/bb_back/settings.py
+++ bb_back/bb_back/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS: List[str] = ['*']
+ALLOWED_HOSTS = ['*']
 
 # Environs
 DATABASE_NAME = env('DATABASE_NAME')
