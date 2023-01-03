2a7e9a82cbfa1560b9e541c48886903a27ae1607
Artem Kalugin <jun-python@omnic.com>
2023-01-03 21:37:45 +0300

    BB-emails - typo fix

--- bb_back/bb_back/urls.py
+++ bb_back/bb_back/urls.py
@@ -50,7 +50,7 @@ urlpatterns = [
     re_path(f'{API_PREFIX}/{API_VERSION}/swagger/',
             schema_view.with_ui('swagger', cache_timeout=0),
             name='schema-swagger-ui'),
-    re_path(f'{API_PREFIX}/{API_VERSION}redoc/',
+    re_path(f'{API_PREFIX}/{API_VERSION}/redoc/',
             schema_view.with_ui('redoc', cache_timeout=0),
             name='schema-redoc'),
 ]
