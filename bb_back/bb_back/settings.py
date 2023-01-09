0ee89e3ea579a954288487e1eaca24fe6cfd2e36
Artem Kalugin <jun-python@omnic.com>
2023-01-09 20:59:14 +0300

    BB pipeline fix

--- bb_back/bb_back/settings.py
+++ bb_back/bb_back/settings.py
@@ -169,7 +169,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"), # Django will search for /src/
+    os.path.join(BASE_DIR, "static"),  # Django will search for /src/
 ]
 CORE_TEMPLATES_PATH = 'bb_back/core/templates'
 
