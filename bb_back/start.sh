b15f8c2506ba70babdaa4713ba91291faad0aceb
Artem Kalugin <jun-python@omnic.com>
2023-01-09 19:57:55 +0300

    BB CI fix

--- bb_back/start.sh
+++ bb_back/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 python manage.py makemigrations
 python manage.py migrate
-python manage.py collectstatic
+python manage.py collectstatic --noinput
 gunicorn bb_back.wsgi:application --bind 0:8000
