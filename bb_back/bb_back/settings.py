c3eafa19bd3340e3eae7eb50c02e9d36c52b225a
Artem Kalugin <jun-python@omnic.com>
2023-01-09 20:17:41 +0300

    BB staticfiles fix

--- bb_back/bb_back/settings.py
+++ bb_back/bb_back/settings.py
@@ -167,6 +167,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 CORE_TEMPLATES_PATH = 'bb_back/core/templates'
 
 # Default primary key field type
