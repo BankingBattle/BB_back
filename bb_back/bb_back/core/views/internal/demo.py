5aeee3b90bc398535920f4ca75df2ecb6f6dade0
Артем Калугин <jun-python@omnicore.ru>
2023-05-09 19:50:48 +0300

    demo fix

--- bb_back/bb_back/core/views/internal/demo.py
+++ bb_back/bb_back/core/views/internal/demo.py
@@ -1,3 +1,4 @@
+from rest_framework import status as drf_status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -5,4 +6,5 @@ from rest_framework.views import APIView
 class DemoView(APIView):
 
     def get(self, request, status):
-        return Response(data=dict(success=True), status=int(status))
+
+        return Response(data={}, status=drf_status.HTTP_405_METHOD_NOT_ALLOWED)
