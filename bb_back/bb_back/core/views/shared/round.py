e375384ec8c98a7cebcaa898490cd4a2b28a4066
zemlianin <detulie@mail.ru>
2023-05-22 16:45:12 +0300

    fix-peth-gen

--- bb_back/bb_back/core/views/shared/round.py
+++ bb_back/bb_back/core/views/shared/round.py
@@ -20,7 +20,7 @@ from bb_back.core.views.utils.base_serializers import (
     UserRolePermissionDeniedSerializer,
 )
 from bb_back.core.views.utils.decorators import is_staff_user
-from bb_back.settings import SUBMIT_MAX_SIZE
+from bb_back.settings import SUBMIT_MAX_SIZE, MEDIA_ROOT
 
 
 class CreateRoundRequestSerializer(serializers.Serializer):
@@ -263,7 +263,7 @@ class GetRoundDataView(APIView):
                 data={},
                 message=f"Round with id = {round_id} has no data.",
             )
-        if not os.path.exists(round.data_of_round.name):
+        if not os.path.exists(os.path.join(MEDIA_ROOT, round.data_of_round.name)):
             return response(
                 success=False,
                 status_code=status.HTTP_404_NOT_FOUND,
