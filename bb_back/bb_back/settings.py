ee3b0ae3705b0f6d34a0094d0aa5aef091183a56
Артем Калугин <jun-python@omnicore.ru>
2023-05-24 20:22:23 +0300

    CSRF_TRUSTED_ORIGINS added

--- bb_back/bb_back/settings.py
+++ bb_back/bb_back/settings.py
@@ -205,6 +205,8 @@ EMAIL_HOST_USER = "banking.battle.info@gmail.com"
 EMAIL_HOST_PASSWORD = "qesvyewhhdgzefqk"
 EMAIL_PORT = "587"
 EMAIL_USE_TLS = True
-
+CSRF_TRUSTED_ORIGINS = [
+    "https://bankingbattle.ru", "https://*.*", "http://*.*"
+]
 # Submit settings
 SUBMIT_MAX_SIZE = 50000000
